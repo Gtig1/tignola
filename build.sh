286e167b261e6a672789d41ccb1e17d8bb8f936b
Gtig1 <giovanni.tignola@gmail.com>
2025-08-09 13:48:35 +0200

    .0

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+if [ $# -ne 2 ] ; then
+  echo "USAGE: $0 input.ged index.html"
+  exit 1
+fi
+npm install && \
+  node_modules/.bin/parse-gedcom "$1" > src/entries.json && \
+  node_modules/.bin/webpack && \
+  mv dist/index.html "$2" && \
+  rm -r dist && \
+  echo "SUCCESS! The HTML file has been saved to $2."
--- topola/topola-viewer-master/prettier.config.mjs
+++
@@ -1,8 +0,0 @@
-const config = {
-  bracketSpacing: false,
-  endOfLine: 'lf',
-  singleQuote: true,
-  plugins: ['prettier-plugin-organize-imports'],
-};
-
-export default config;
